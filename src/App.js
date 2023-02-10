cc445bdaecb5a55f275e676862f05ab74e16c5fb
shristi-rks <shristi.n08@gmail.com>
2023-02-10 17:20:07 +0200

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-   
-          Learn React
-   
-      </header>
-    </div>
-  );
-}
 
+function App() {
+  const [countries, getCountries] = useState([])
+  const API = 'https://restcountries.com/v3.1/all';
+  const fetchCountriesData = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        getCountries(res)
+      })
+  }
+  useEffect(() => {
+    fetchCountriesData()
+  }, [])
+  return (
+    <>
+      <h2>Countries</h2>
+      <ul>
+        {countries.map((country, i) => {
+          return <li key={i}>{country.name.common}</li>
+        })}
+      </ul>
+    </>
+  )
+      }
+ 
 export default App;
---
+++ src/Component/Homepage.jsx
@@ -0,0 +1,10 @@
+function Homepage({countries, key}) {
+    return (
+        <div>
+            <h1>countries.name.common</h1>
+        </div>
+    )
+
+}
+
+export default Homepage;
