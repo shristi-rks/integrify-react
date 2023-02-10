776030a1db43f9be180d24ef3879ea5f719b4428
shristi-rks <shristi.n08@gmail.com>
2023-02-10 15:25:05 +0200

    install bootstrap

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Integrify's react assignment
 
 -create react app
--
+-make Homepage component
+-import react-bootstrap
+-make Navbar component
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// react bootstrap configuration
+import '../node_modules/react-bootstrap/dist/react-bootstrap'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
