df834a487673712cae37acf7038996d66803eb24
shristi-rks <shristi.n08@gmail.com>
2023-02-13 19:32:37 +0200

    add items per page menu

--- src/Component/PaginationedTable.jsx
+++ src/Component/PaginationedTable.jsx
@@ -4,9 +4,12 @@ import CountriesTable from "./CountriesTable";
 
 export const PaginatedTable= ({countries}) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 5;
-    const handlePageUp = (pageNumber) => setCurrentPage(pageNumber+1);
-    const handlePageDown = (pageNumber) => setCurrentPage(pageNumber-1);
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+    const handlePageUp = (pageNumber) => setCurrentPage(pageNumber+1)
+     
+    const handlePageDown = (pageNumber) => {
+        setCurrentPage(pageNumber -1);   
+    }
   
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -16,14 +19,24 @@ export const PaginatedTable= ({countries}) => {
     for (let i = 1; i <= Math.ceil(countries.length / itemsPerPage); i++) {
       pageNumbers.push(i);
     }
+    const handleItemsPerPage = (e) => {
+      setItemsPerPage(e.target.value)
+    }
 
     return (
         <>
         <CountriesTable currentItem={currentItems} />
         <Pagination>
         <p>Flows per page</p>
+        <select onChange= {handleItemsPerPage}>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={25}>25</option>
+          <option value={50}>50</option>
+        </select>
           <Pagination.Item> {indexOfFirstItem + 1}-{indexOfLastItem}</Pagination.Item>
-          <Pagination.Prev onClick={() => handlePageDown(currentPage)}/>
+          <Pagination.Prev disabled ={currentPage ===1} onClick={() => handlePageDown(currentPage)}/>
           <Pagination.Next onClick={() => handlePageUp(currentPage)}/>
           </Pagination>
           </>
